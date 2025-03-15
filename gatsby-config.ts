1136eca69b3b9def53614714ddd0a91eb3e33def
Liam Morley <liam@carpeliam.com>
2025-03-15 14:47:07 -0400

    Add analytics

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -11,12 +11,21 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ['gatsby-plugin-postcss', 'gatsby-plugin-image', 'gatsby-plugin-sitemap', {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      icon: 'src/images/icon.png'
-    }
-  }, 'gatsby-plugin-mdx', 'gatsby-plugin-sharp', 'gatsby-transformer-sharp',
+  plugins: [
+    'gatsby-plugin-postcss', 'gatsby-plugin-image', 'gatsby-plugin-sitemap',
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: ['G-RRV7K7BCCR'],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.png',
+      },
+    },
+    'gatsby-plugin-mdx', 'gatsby-plugin-sharp', 'gatsby-transformer-sharp',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
