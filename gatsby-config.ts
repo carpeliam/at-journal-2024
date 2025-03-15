6979338fbde770cb3c2d358124dfda388cfdb1f6
Liam Morley <liam@carpeliam.com>
2025-03-15 01:17:00 -0400

    Add markdown formatting plugins

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,103 +1,88 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: 'Marathon From Georgia To Maine',
     description: 'Between April 3rd and August 13th of 2024, I hiked 2197 miles from Georgia to Maine. This is the story of the steps.',
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: 'https://www.yourdomain.tld'
   },
-  pathPrefix: "/at-journal-2024",
+  pathPrefix: '/at-journal-2024',
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: ['gatsby-plugin-postcss', 'gatsby-plugin-image', 'gatsby-plugin-sitemap', {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      "icon": "src/images/icon.png"
+      icon: 'src/images/icon.png'
     }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  }, 'gatsby-plugin-mdx', 'gatsby-plugin-sharp', 'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/'
+      },
+      __key: 'images'
     },
-    __key: "images"
-  },
-  {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: './src/pages/'
+      },
+      __key: 'pages'
     },
-    __key: "pages"
-  },
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `content`,
-      path: "./src/content/",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'content',
+        path: './src/content/',
+      },
     },
-  },
-  {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "geojson",
-      "path": "./src/geojson/"
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'geojson',
+        path: './src/geojson/'
+      },
+      __key: 'geojson'
     },
-    __key: "geojson"
-  },
-  {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "entries",
-      "path": "./src/entries/"
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'entries',
+        path: './src/entries/'
+      },
+      __key: 'entries'
     },
-    __key: "entries"
-  },
-  {
-    resolve: `gatsby-transformer-remark`,
-    options: {
-      // gfm: true,
-      // footnotes: true,
-      // blocks: ["h2"], Blocks option value can be provided here as an array.
-      // excerpt_separator: `<!-- end -->`,
-      plugins: [
-        // {
-        //   resolve: `gatsby-remark-images`,
-        //   options: {
-        //     maxWidth: 740,
-        //   },
-        // },
-        // {
-        //   resolve: `gatsby-remark-responsive-iframe`,
-        //   options: {
-        //     wrapperStyle: `margin-bottom: 1.0725rem`,
-        //   },
-        // },
-        // `gatsby-remark-copy-linked-files`,
-        // {
-        //   resolve: `gatsby-remark-smartypants`,
-        //   options: {
-        //     dashes: `oldschool`,
-        //   },
-        // },
-        // {
-        //   resolve: `gatsby-remark-embed-snippet`,
-        //   options: {
-        //     // Example code links are relative to this dir.
-        //     directory: `${__dirname}/src/code-examples/`,
-        //   },
-        // },
-        // `gatsby-remark-autolink-headers`,
-        // `gatsby-remark-graphviz`, // graphviz before prismjs
-        // `gatsby-remark-prismjs`,
-        // `gatsby-remark-katex`,
-      ],
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        // gfm: true,
+        // footnotes: true,
+        // blocks: ['h2'], Blocks option value can be provided here as an array.
+        // excerpt_separator: '<!-- end -->',
+        plugins: [
+          'gatsby-remark-smartypants',
+          {
+            resolve: 'gatsby-remark-external-links',
+            options: {
+              rel: 'noopener noreferrer',
+            },
+          },
+          // {
+          //   resolve: 'gatsby-remark-images',
+          //   options: {
+          //     maxWidth: 740,
+          //   },
+          // },
+          // 'gatsby-remark-autolink-headers',
+        ],
+      },
     },
-  },
-  'gatsby-plugin-react-leaflet'
-]
+    'gatsby-plugin-react-leaflet'
+  ]
 };
 
 export default config;
