9539ae2b93ae738089448ab2793bde74caa95914
Liam Morley <liam@carpeliam.com>
2025-03-20 01:20:27 -0400

    Updates

---
+++ cfaebb58-be27e44b79fda1049cce.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkat_2024_day_by_day=self.webpackChunkat_2024_day_by_day||[]).push([[957],{2169:function(t,a,n){n.d(a,{R8s:function(){return e}});var c=n(2225);function e(t){return(0,c.k5)({tag:"svg",attr:{version:"1",viewBox:"0 0 48 48",enableBackground:"new 0 0 48 48"},child:[{tag:"path",attr:{fill:"#4CAF50",d:"M8,12h22c2.2,0,4,1.8,4,4v16c0,2.2-1.8,4-4,4H8c-2.2,0-4-1.8-4-4V16C4,13.8,5.8,12,8,12z"},child:[]},{tag:"polygon",attr:{fill:"#388E3C",points:"44,35 34,29 34,19 44,13"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=cfaebb58-be27e44b79fda1049cce.js.map
