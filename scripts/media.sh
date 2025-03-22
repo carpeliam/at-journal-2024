9428e49655cfffa0299aa865478e29cc3794b593
Liam Morley <liam@carpeliam.com>
2025-03-22 16:50:40 -0400

    Run media shell script on any directory, with or without files of both types
    - Copies files to absolute path for src/images, regardless of where $mediasrc/$1 are located
    - Bypasses execution (ie doesn't die) in cases where no images or movies are found

--- scripts/media.sh
+++ scripts/media.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+root_dir=$(dirname $(dirname $(realpath "$0")))
 mediasrc=$1
 ## Uncomment to create a backup of existing images:
 # timestamp=$(date +%Y%m%d_%H%M%S)
@@ -11,20 +12,29 @@ mediasrc=$1
 # TODO perhaps automate export from MacOS Photos via https://rhettbull.github.io/osxphotos/cli.html#osxphotos-export
 
 pushd $mediasrc
-mkdir -p handbrake
-for movie in *.mov; do
-    HandbrakeCLI --preset "Social 25 MB 1 Minute 720p60" --input ${movie} --output "handbrake/${movie%.mov}.mp4" --optimize
-    exiftool -TagsFromFile ${movie} -extractEmbedded -all:all -FileCreateDate -GPSPosition "handbrake/${movie%.mov}.mp4" -overwrite_original
-done
+
+if [ -e *.mov ]; then
+    mkdir -p handbrake
+    for movie in *.mov; do
+        HandbrakeCLI --preset "Social 25 MB 1 Minute 720p60" --input ${movie} --output "handbrake/${movie%.mov}.mp4" --optimize
+        exiftool -TagsFromFile ${movie} -extractEmbedded -all:all -FileCreateDate -GPSPosition "handbrake/${movie%.mov}.mp4" -overwrite_original
+    done
+
+    mv handbrake/* "${root_dir}/src/images"
+else
+    echo "No movies found."
+fi
 
 # mkdir -p imageoptim
 # cp -p *.jpeg imageoptim
 # printf "%s " "Run ImageOptim on all files in ${mediasrc}/imageoptim, then press Enter"
 # read
-mkdir -p jpegoptim
-jpegoptim --dest=jpegoptim --overwrite --preserve *.jpeg
-
-mv handbrake/* ../src/images
-mv jpegoptim/* ../src/images
+if [ -e *.jpeg ]; then
+    mkdir -p jpegoptim
+    jpegoptim --dest=jpegoptim --overwrite --preserve *.jpeg
+    mv jpegoptim/* "${root_dir}/src/images"
+else
+    echo "No images found."
+fi
 
 popd
