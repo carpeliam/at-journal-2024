72e68c44e28a8aa0385b5cbae12aec5ac3913fa2
Liam Morley <liam@carpeliam.com>
2025-03-23 23:54:27 -0400

    Use exifr for parsing image exif data
    - it seems to be a wee bit faster than fast-exif, on average
    - it calculates latitude/longitude in decimal
    - it can return creation date as a string, for easier/safer parsing

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
-import { read as readExif } from 'fast-exif';
-import exiftool from 'exiftool';
 import type { CreateNodeArgs, GatsbyNode } from 'gatsby';
+import { parse as parseExif } from 'exifr';
+import exiftool from 'exiftool';
 
 type SiteNode = Record<string, unknown> & {
   frontmatter: {
@@ -19,7 +19,7 @@ export const onCreateNode: GatsbyNode["onCreateNode"] = async ({ node, actions }
         if (err)
           console.error(err);
         else {
-          exiftool.metadata(data, ['-gpsLatitude', '-gpsLongitude', '-creationDate'], (err, { gpsLatitude, gpsLongitude, creationDate }) => {
+          exiftool.metadata(data, ['-creationDate', '-gpsLatitude', '-gpsLongitude', '-coordFormat', '%.4f'], (err, { creationDate, gpsLatitude, gpsLongitude }) => {
             if (err)
               console.error(err);
             else {
@@ -31,42 +31,55 @@ export const onCreateNode: GatsbyNode["onCreateNode"] = async ({ node, actions }
                 });
               }
               if (gpsLatitude && gpsLongitude) {
+                const latitude = parseFloat(gpsLatitude) * (gpsLatitude.endsWith('N') ? 1 : -1);
+                const longitude = parseFloat(gpsLongitude) * (gpsLongitude.endsWith('E') ? 1 : -1);
                 createNodeField({
                   node,
                   name: 'coordinates',
                   value: {
-                    latitude: degreesMinutesSecondsStringToLatLng(gpsLatitude),
-                    longitude: degreesMinutesSecondsStringToLatLng(gpsLongitude),
+                    latitude,
+                    longitude,
                   }
                 });
               } else {
-                console.debug('no GPS data for', node.relativePath, creationDate);
+                console.debug('no GPS data for', node.relativePath);
               }
             }
           });
         }
       });
     } else {
-      const exif = await readExif(node.absolutePath).catch((e) => { console.error('Failed to read', node.relativePath) });
-      
-      if (exif?.exif?.DateTimeOriginal) {
-        const dateTimeOriginal = exif.exif.DateTimeOriginal as Date;
-        const dateTimeOriginalOffset = exif.exif['36881'] as string | undefined;
-        const value = (dateTimeOriginalOffset) ? new Date(dateTimeOriginal.toISOString().replace('Z', dateTimeOriginalOffset)) : dateTimeOriginal;
+      const exif = await parseExif(node.absolutePath, {
+        reviveValues: false,
+        pick: ['DateTimeOriginal', 'OffsetTimeOriginal', 'GPSLatitude', 'GPSLatitudeRef', 'GPSLongitudeRef', 'GPSLongitude'],
+      }).catch((e) => { console.error('Failed to read', node.relativePath) });
+
+      if (exif?.DateTimeOriginal) {
+        let offsetTimeOriginal: string;
+        if (exif.OffsetTimeOriginal) {
+          offsetTimeOriginal = exif.OffsetTimeOriginal;
+        } else {
+          const localOffset = defaultClientOffset();
+          console.warn('no timezone specified for', node.relativePath, ', using local offset of', localOffset, 'instead');
+          offsetTimeOriginal = localOffset;
+        }
+        const value = parseExifDate(exif.DateTimeOriginal + offsetTimeOriginal);
         createNodeField({
           node,
           name: 'createDate',
           value,
         });
+      } else {
+        console.debug('no creation date data for', node.relativePath);
       }
-      if (exif?.gps) {
+      if (exif?.latitude && exif?.longitude) {
         createNodeField({
           node,
           name: 'coordinates',
           value: {
-            latitude: decimalLatitudeFor(exif.gps),
-            longitude: decimalLongitudeFor(exif.gps),
-          }
+            latitude: exif.latitude,
+            longitude: exif.longitude,
+          },
         });
       } else {
         console.debug('no GPS data for', node.relativePath);
@@ -90,43 +103,17 @@ export const onCreateWebpackConfig: GatsbyNode["onCreateWebpackConfig"] = ({ sta
   }
 }
 
-function degreesMinutesSecondsStringToLatLng(dms: string): number | undefined {
-  const [_whole, degrees, minutes, seconds, direction] = dms.match(/(\d+) deg (\d+)' (\d+.?\d+)" ([NSEW])/)!;
-  const degreesMinutesSeconds = [degrees, minutes, seconds].map(parseFloat);
-  if (['N', 'S'].includes(direction)) {
-    return decimalLatitudeFor({ GPSLatitudeRef: direction, GPSLatitude: degreesMinutesSeconds });
-  } else if (['E', 'W'].includes(direction)) {
-    return decimalLongitudeFor({ GPSLongitudeRef: direction, GPSLongitude: degreesMinutesSeconds });
-  }
-  console.debug('no coordinates determined for', dms);
-}
-
-function degreesMinutesSecondsToDecimal([degrees, minutes, seconds]: number[]) {
-  return degrees + (minutes / 60) + (seconds / 3600);
-}
-
-function decimalLatitudeFor(gps: Record<string, unknown>) {
-  if (gps?.GPSLatitude) {
-    const decimal = degreesMinutesSecondsToDecimal(gps.GPSLatitude as number[]);
-    return (gps.GPSLatitudeRef === 'N') ? decimal : -decimal;
-  } else {
-    console.debug('no latitude for', gps);
-  }
-}
-
-function decimalLongitudeFor(gps: Record<string, unknown>) {
-  if (gps?.GPSLongitude) {
-    const decimal = degreesMinutesSecondsToDecimal(gps.GPSLongitude as number[]);
-    return (gps.GPSLongitudeRef === 'E') ? decimal : -decimal;
-  } else {
-    console.log('no longitude for', gps);
-  }
+function defaultClientOffset() {
+  return Intl.DateTimeFormat('en', { timeZoneName: 'longOffset' })
+    .formatToParts(new Date())
+    .find(part => part.type === 'timeZoneName')!.value
+    .replace('GMT', '');
 }
 
 // currently unable to use exif-date library until https://github.com/blakeembrey/exif-date/pull/18 is addressed
 const EXIF_DATE_REGEXP = new RegExp(
   "^(\\d{4}):(\\d{2}):(\\d{2}) (\\d{2}):(\\d{2}):(\\d{2})" +
-    "(?:\\.(\\d{1,3}))?(?:([zZ])|([-+])(\\d{2}):(\\d{2}))?$"
+  "(?:\\.(\\d{1,3}))?(?:([zZ])|([-+])(\\d{2}):(\\d{2}))?$"
 );
 function parseExifDate(value: string): Date {
   const m = EXIF_DATE_REGEXP.exec(value);
@@ -139,8 +126,8 @@ function parseExifDate(value: string): Date {
   const offset = m[8]
     ? 0
     : m[9]
-    ? (Number(m[10]) * 60 + Number(m[11])) * (m[9] === "+" ? -1 : 1)
-    : 0;
+      ? (Number(m[10]) * 60 + Number(m[11])) * (m[9] === "+" ? -1 : 1)
+      : 0;
 
   date.setUTCFullYear(Number(m[1]));
   date.setUTCMonth(Number(m[2]) - 1);
